8b2280b958d0fee778675d7f8c83cca1a78a318e
jagerua <jagerua03@gmail.com>
2023-10-06 20:23:57 +0300

    Create Game Board. Create players and turns for them. Add some logic behind the game. Updata score. Check for Win. Change color depending on the player turn.

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
+const playerColor = ['red', 'blue'];
+
 const options = {
     ready: false,
     enemy: '',
-    symbol: '',
+    symbol: 'x',
+    color: playerColor[random(playerColor.length)],
 };
 
 const balloons = document.querySelector('.balloons');
 
-//Get random number
+// Get random number.
 function random(num) {
     return Math.floor(Math.random() * num);
 };
 
+// Get random numbers in the between of two.
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 };
 
+// Style ballons with random values.
 function randomStylesBallons() {
     var mt = random(200);
     var ml = random(50);
@@ -26,7 +31,7 @@ function randomStylesBallons() {
     `;
 };
 
-//Create ballons
+// Create ballons.
 function createBalloons (num) {
     for (let i = num; i > 0; i--) {
         const balloon = document.createElement('img');
@@ -39,15 +44,17 @@ function createBalloons (num) {
 
 //Start function
 window.addEventListener("load", () => {
+    // Numbers of ballons on the main page.
     createBalloons(50);
 });
 
 // ==============================================
 
 const optionBtns = document.querySelectorAll('.opponent-btn');
-
+// Blood drops in button animation
 const numberOfDrops = 12;
-    
+
+// Rendering blood drops
 function createDrops(num, el) {
     for (let i = num; i > 0; i--) {
         const drop = document.createElement('span');
@@ -57,6 +64,7 @@ function createDrops(num, el) {
     }
 };
 
+// Random style drops so they never the same.
 function randomStylesDrops() {
     var mt = random(6);
     var dur = randomNumber(0.4, 1);
@@ -68,6 +76,7 @@ function randomStylesDrops() {
     `;
 };
 
+// Choose opponent and add value to the object. Set the selected button to active.
 function chooseOpponent(e) {
     optionBtns.forEach(el => {
         el.innerHTML = el.innerText;
@@ -89,7 +98,7 @@ function chooseOpponent(e) {
 
 const symbolBtns = document.querySelectorAll('.symbol-btn');
 
-// 
+// Choose symbol and add value to the object. Set the selected button to active.
 function chooseSymbol(e) {
     symbolBtns.forEach(el => {
         el.innerHTML = el.innerText;
@@ -114,7 +123,8 @@ function readyForGame () {
     const playBtn = document.querySelector('.play');
     const wave = document.querySelector('.wave');
 
-    // Check if every option is present to show button
+    // Check if every option is present to show button and wave animation.
+    // Emeny & symbol should be selected to start.
     if (options.enemy && options.symbol) {
         playBtn.style.cssText += 'animation:boat 3.5s both';
         wave.style.cssText += 'animation-name: ripple;-moz-animation-delay: 3.5s;-webkit-animation-delay: 3.5s;animation-delay: 3.5s;-moz-animation-duration: 2s;-webkit-animation-duration: 2s;animation-duration: 2s;-moz-animation-iteration-count: infinite;-webkit-animation-iteration-count: infinite;animation-iteration-count: infinite;'
@@ -125,6 +135,7 @@ readyForGame();
 
 // ===============================================
 
+// Array of pics for the loading animation.
 const hands = [
     {
         name: '1',
@@ -168,22 +179,26 @@ const hands = [
     },
 ];
 
+// Min nubmer of hands (cards) rows on loading screen.
 let rows = 4;
 window.addEventListener("resize", () => renderHands(hands, rows));
 
+// Rander hands (cards) on loading screen.
 function renderHands(arr, rows) {
+    // One card heaight
     let height = 140;
+    // Change number of rows depends on the screen height
     rows = Math.ceil(window.innerHeight / height);
     document.querySelector('.hands').innerHTML = '';
 
-
+    // Creating row with hands (cards).
     for (let i = 0; i < rows; i++) {
         let width = 0;
         const row = document.createElement('div');
         row.className = `row`;
         row.style.cssText = `height: ${height}` + 'px';
         document.querySelector('.hands').appendChild(row);
-
+        // Creating single hand (card) with some styles.
         for (let j = 0; j < arr.length; j++){
             const item = document.createElement('div');
             item.className = 'item';
@@ -195,8 +210,10 @@ function renderHands(arr, rows) {
     }
 };
 
-renderHands(hands, rows);
+// renderHands(hands, rows);
 
+// Set some random styles for every hand (card) in the row.
+// Every reload it`s going to be different. 
 function randomStyleForHands(w) {
     return `
         position: absolute;
@@ -205,4 +222,146 @@ function randomStyleForHands(w) {
         transform: rotate(${randomNumber(9, -9)}deg);
         z-index: ${randomNumber(-1, 1)};
     `;
+};
+
+//==============================================================
+// Треба переробити щоб щапуск був після анімації
+window.addEventListener('load', start);
+
+// Визначити гравців і символи
+// Визначити чия черга
+// Записати хід в масив
+// Перевірити чи хтось виграв або чи нічья
+
+// Player turn
+let turn = options.symbol;
+
+const playerOne = {
+    symbol: options.symbol,
+    color: options.color,
+};
+
+const playerTwo = {
+    symbol: playerOne.symbol == 'x' ? 'o' : 'x',
+    color: playerOne.color == 'red' ? 'blue' : 'red',
+};
+
+// Empty game board.
+const gameBoard = ['','','','','','','','',''];
+// Winning combinations, all posible.
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const gameTable = document.querySelector('.game-table');
+
+// Rander board and add click to it.
+function randerBoard () {
+    gameBoard.forEach((el, index) => {
+        const div = document.createElement('div');
+        div.className = 'cell';
+        div.setAttribute('data-number', index);
+        div.addEventListener('click', go, { once: true });
+        gameTable.appendChild(div);
+    });
+};
+
+const thePlayer = document.querySelector('.player-one');
+const theEnemy = document.querySelector('.player-two');
+
+let playerScore = document.querySelector('.player-one-wins');
+let enemyScore = document.querySelector('.player-two-wins');
+// Score counter
+let playerWins = 0;
+let enemyWins = 0;
+
+// Set rendom color to each player
+function setGameColors(options) {
+    if (options.color && options.color !== '') {
+        thePlayer.classList.add(`player-${options.color}`);
+        theEnemy.classList.add(`player-` + playerColor.filter(item => item != options.color).toString()); // playerOne.color == 'red' ? 'blue' : 'red',
+    }
+};
+
+// 
+function go() {
+    this.classList.add(turn);
+
+    // Check if we win
+    if (checkWin(turn)) {
+        alert('Winner is ' + turn.toUpperCase());
+        updateScore(turn);
+        start(turn);
+    }
+
+    // Check whos turn it is and draw symbol
+    if (turn == 'o') {
+        const div = document.createElement('div');
+        div.className = `circle circle-${playerOne.color}`;
+        this.appendChild(div);
+        let dataIndex = this.getAttribute('data-number');
+        gameBoard.splice(dataIndex, 1, 'o');
+
+        turn = 'x';
+        showColorBaseOnTurn(turn);
+    } else {
+        const div = document.createElement('div');
+        div.className = `cross cross-${playerTwo.color}`;
+        this.appendChild(div);
+        let dataIndex = this.getAttribute('data-number');
+        gameBoard.splice(dataIndex, 1, 'x');
+
+        turn = 'o';
+        showColorBaseOnTurn(turn);
+    }
+    hoverTheCell(turn, this);
+};
+
+// Updating the score
+function updateScore(turn) {
+    turn == 'x' ? playerScore.innerHTML = playerWins += 1 : enemyScore.innerHTML = enemyWins += 1;
+};
+
+// Notify the Player thats it`s his turn by color of avatar.
+function showColorBaseOnTurn(turn) {
+    if (turn == 'x') {
+        thePlayer.style.filter = 'grayscale(0.8)';
+        theEnemy.style.filter = 'grayscale(0)';
+    } else {
+        theEnemy.style.filter = 'grayscale(0.8)';
+        thePlayer.style.filter = 'grayscale(0)';
+    }
+};
+
+// Hover cell depending on the symbol wich go next.
+function hoverTheCell(turn, item) {
+    gameTable.classList.remove('o');
+    gameTable.classList.remove('x');
+
+    gameTable.classList.add(turn);
+};
+
+// 
+function checkWin(turn) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            let cellEl = document.querySelectorAll('.cell');
+            return cellEl[index].classList.contains(turn);
+        })
+    })
+};
+
+// Start function
+function start() {
+    gameTable.innerHTML = '';
+    randerBoard();
+    setGameColors(options);
+    hoverTheCell(turn);
 };
